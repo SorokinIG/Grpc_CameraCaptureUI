1afddc51020e8a99b7a33b8f44e26de933f16aa5
nellepost5 <68441532+nellepost5@users.noreply.github.com>
2023-04-12 14:23:11 +0300

    Добавьте файлы проекта.

---
+++ Grpc_webcamera/Program.cs
@@ -0,0 +1,18 @@
+using Grpc_webcamera;
+using Grpc_webcamera.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<VideoService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ Grpc_webcamera/VideoService.cs
@@ -0,0 +1,56 @@
+﻿//CameraCaptureUI
+using Grpc.Core;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+
+
+namespace Grpc_webcamera
+{
+    public class VideoService:Messenger.MessengerBase
+    {
+        string[] messages = {  };
+     
+
+        public VideoService()
+        {
+            
+            
+            
+          
+        }
+
+        public override async Task DataStream(IAsyncStreamReader<Request> requestStream,
+            IServerStreamWriter<Response> responseStream,
+            ServerCallContext context)
+        {
+            // считываем входящие сообщения в фоновой задаче
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (Request message in requestStream.ReadAllAsync())
+                {
+                    Console.WriteLine($"Client: {message.Content}");
+                }
+            });
+
+            while (true)
+            {
+                // Посылаем ответ, пока клиент не закроет поток
+                //  if (!readTask.IsCompleted)
+                //  {                              
+                    if ()
+                {
+                   
+                    await responseStream.WriteAsync(new Response { Data = );                                        
+                }                   
+               // }
+            }
+            await readTask; // ожидаем завершения задачи чтения
+        }
+
+
+
+       
+        
+    }
+}
